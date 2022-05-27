c5c8d6b7cdec5540d670b1f77d12fd94e417c7a5
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-05-27 21:54:13 +0700

    Base project

---
+++ src/main/java/com/example/vlmart/SpringBootVlmartManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.vlmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVlmartManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVlmartManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vlmart/SpringBootVlmartManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vlmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVlmartManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
