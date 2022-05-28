b7a07a3efbc985a092fff13d1fb980765853a980
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-05-28 14:45:37 +0700

    Update project base

---
+++ src/main/java/com/example/vlmart/config/RunnerConfig.java
@@ -0,0 +1,16 @@
+package com.example.vlmart.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RunnerConfig implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
