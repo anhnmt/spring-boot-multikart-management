c84ea44223854e35680ce76e3b727297d1aa25fe
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-05-28 15:32:03 +0700

    Update application config

--- src/main/java/com/example/vlmart/controller/HomeController.java
+++ src/main/java/com/example/vlmart/controller/HomeController.java
@@ -3,11 +3,13 @@ package com.example.vlmart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String homePage(Model model) {
         return "index";
     }
