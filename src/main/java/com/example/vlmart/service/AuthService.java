13b2d04a17bcd8b30e179db3deab24fbd282a318
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-05-30 23:51:13 +0700

    Update demo login user

--- src/main/java/com/example/vlmart/service/AuthService.java
+++ src/main/java/com/example/vlmart/service/AuthService.java
@@ -1,8 +1,15 @@
 package com.example.vlmart.service;
 
+import com.example.vlmart.domain.dto.UserLoginRequestDTO;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 public interface AuthService {
 
-    String backendLogin();
+    String backendLogin(Model model);
 
-    String backendPostLogin();
+    String backendPostLogin(@Valid UserLoginRequestDTO userDTO, HttpServletRequest request, BindingResult result, Model model);
 }
