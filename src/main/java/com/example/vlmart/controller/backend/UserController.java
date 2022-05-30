eae1264ede4ee297f26238b2ddfc813494fe8968
doremon04 <minhnt141204@gmail.com>
2022-05-30 23:57:18 +0700

    Update create user

--- src/main/java/com/example/vlmart/controller/backend/UserController.java
+++ src/main/java/com/example/vlmart/controller/backend/UserController.java
@@ -1,12 +1,16 @@
 package com.example.vlmart.controller.backend;
 
+import com.example.vlmart.domain.model.User;
 import com.example.vlmart.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/dashboard/users")
 public class UserController {
@@ -14,8 +18,19 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public String findAllUsers(Model model) {
+    public String index(Model model) {
         return userService.findAllUsers(model);
     }
 
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("user", new User());
+        return "backend/user/create";
+    }
+
+    @PostMapping
+    public String store(User user) {
+        log.info(user.toString());
+        return userService.storeUser(user);
+    }
 }
--- src/main/java/com/example/vlmart/service/UserService.java
+++ src/main/java/com/example/vlmart/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.vlmart.service;
 
+import com.example.vlmart.domain.model.User;
 import org.springframework.ui.Model;
 
 public interface UserService {
     String findAllUsers(Model model);
+    String storeUser(User user);
 }
