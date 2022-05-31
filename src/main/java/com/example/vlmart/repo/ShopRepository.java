848f02dc7265ef1142a24d22218ce6b6320f0667
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-05-31 19:44:53 +0700

    Remove shops

--- src/main/java/com/example/vlmart/repo/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vlmart.repo;
-
-import com.example.vlmart.domain.model.Shop;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShopRepository extends CrudRepository<Shop, Long> {
-}
