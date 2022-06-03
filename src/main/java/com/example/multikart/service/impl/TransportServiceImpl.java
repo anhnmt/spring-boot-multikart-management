e90ede25bd929144c6b95daf0a8e35b0742d1a63
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-06-03 13:01:37 +0700

    Update crud transport

--- src/main/java/com/example/multikart/service/impl/TransportServiceImpl.java
+++ src/main/java/com/example/multikart/service/impl/TransportServiceImpl.java
@@ -1,12 +1,127 @@
 package com.example.multikart.service.impl;
 
+import com.example.multikart.common.Const;
+import com.example.multikart.common.DataUtils;
+import com.example.multikart.domain.dto.TransportRequestDTO;
+import com.example.multikart.domain.model.Transport;
 import com.example.multikart.repo.TransportRepository;
 import com.example.multikart.service.TransportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Service
 public class TransportServiceImpl implements TransportService {
     @Autowired
     private TransportRepository transportRepository;
+
+    @Override
+    public String findAllTransports(Model model) {
+        var transports = transportRepository.findAllByStatus(Const.DefaultStatus.ACTIVE);
+        model.addAttribute("transports", transports);
+
+        return "backend/transport/index";
+    }
+
+    @Override
+    public String createTransport(Model model) {
+        model.addAttribute("transport", Transport.builder().status(Const.DefaultStatus.ACTIVE).build());
+
+        return "backend/transport/create";
+    }
+
+    @Override
+    public String storeTransport(TransportRequestDTO input, BindingResult result, Model model, RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            model.addAttribute("transport", input);
+
+            return "backend/transport/create";
+        }
+
+        var count = transportRepository.countByNameAndStatus(input.getName(), Const.DefaultStatus.ACTIVE);
+        if (count > 0) {
+            result.rejectValue("name", "", "Tên đơn vị đã được sử dụng");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("transport", input);
+
+            return "backend/transport/create";
+        }
+
+        var newTransport = new Transport(input);
+        transportRepository.save(newTransport);
+
+        redirect.addFlashAttribute("success", "Thêm thành công");
+        return "redirect:/dashboard/transports";
+    }
+
+    @Override
+    public String editTransport(Long id, Model model, RedirectAttributes redirect) {
+        var transport = transportRepository.findByTransportIdAndStatus(id, Const.DefaultStatus.ACTIVE);
+        if (DataUtils.isNullOrEmpty(transport)) {
+            redirect.addFlashAttribute("error", "Đơn vị không tồn tại");
+
+            return "redirect:/dashboard/transports";
+        }
+
+        model.addAttribute("transport", transport);
+
+        return "backend/transport/edit";
+    }
+
+    @Override
+    public String updateTransport(Long id, TransportRequestDTO input, BindingResult result, Model model, RedirectAttributes redirect) {
+        var transport = transportRepository.findByTransportIdAndStatus(id, Const.DefaultStatus.ACTIVE);
+        if (DataUtils.isNullOrEmpty(transport)) {
+            redirect.addFlashAttribute("error", "Đơn vị không tồn tại");
+
+            return "redirect:/dashboard/transports";
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("transport", input);
+
+            return "backend/transport/create";
+        }
+
+        if (!transport.getName().equals(input.getName())) {
+            var count = transportRepository.countByNameAndStatus(input.getName(), Const.DefaultStatus.ACTIVE);
+            if (count > 0) {
+                result.rejectValue("name", "", "Tên đơn vị đã được sử dụng");
+            }
+
+            transport.setName(input.getName());
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("transport", input);
+
+            return "backend/transport/create";
+        }
+
+        transport.setStatus(input.getStatus());
+        transportRepository.save(transport);
+
+        redirect.addFlashAttribute("success", "Sửa thành công");
+        return "redirect:/dashboard/transports";
+    }
+
+    @Override
+    public String deleteTransport(Long id, Model model, RedirectAttributes redirect) {
+        var transport = transportRepository.findByTransportIdAndStatus(id, Const.DefaultStatus.ACTIVE);
+        if (DataUtils.isNullOrEmpty(transport)) {
+            redirect.addFlashAttribute("error", "Đơn vị không tồn tại");
+
+            return "redirect:/dashboard/transports";
+        }
+
+        transport.setStatus(Const.DefaultStatus.DELETED);
+        transportRepository.save(transport);
+
+        redirect.addFlashAttribute("success", "Xóa thành công");
+        return "redirect:/dashboard/transports";
+    }
 }
