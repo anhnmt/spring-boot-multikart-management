f4ef98804969586ec04ac05b02123dde2626baa5
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-06-10 22:28:02 +0700

    Update checkout

---
+++ src/main/java/com/example/multikart/controller/CheckoutController.java
@@ -0,0 +1,23 @@
+package com.example.multikart.controller;
+
+import com.example.multikart.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/checkout")
+public class CheckoutController {
+    @Autowired
+    private CheckoutService checkoutService;
+
+    @GetMapping
+    public String view(HttpSession session, Model model) {
+        return checkoutService.view(session, model);
+    }
+
+}
