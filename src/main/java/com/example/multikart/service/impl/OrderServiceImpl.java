3eec2eb38c308b75321794d486907d4e82ab20be
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-06-26 11:02:19 +0700

    Hotfix order details address

--- src/main/java/com/example/multikart/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/multikart/service/impl/OrderServiceImpl.java
@@ -1,11 +1,9 @@
 package com.example.multikart.service.impl;
 
-import com.example.multikart.common.Const;
 import com.example.multikart.common.Const.DefaultStatus;
 import com.example.multikart.common.Const.OrderStatus;
 import com.example.multikart.common.DataUtils;
 import com.example.multikart.common.Utils;
-import com.example.multikart.domain.model.Customer;
 import com.example.multikart.repo.OrderDetailRepository;
 import com.example.multikart.repo.OrderRepository;
 import com.example.multikart.service.OrderService;
@@ -33,7 +31,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public String viewOrder(Long id, Model model, RedirectAttributes redirect) {
-        var order = orderRepository.findByOrderIdAndStatusNot(id, OrderStatus.DELETED);
+        var order = orderRepository.findByOrderIdAndStatusNot(id, OrderStatus.DELETED, DefaultStatus.ACTIVE);
         if (DataUtils.isNullOrEmpty(order)) {
             redirect.addFlashAttribute("error", "Hóa đơn không tồn tại");
 
@@ -42,7 +40,7 @@ public class OrderServiceImpl implements OrderService {
 
         model.addAttribute("order", order);
 
-        var orderDetails = orderDetailRepository.findAllByOrderIdStatusNot(id, OrderStatus.DELETED);
+        var orderDetails = orderDetailRepository.findAllByOrderIdAndStatusNot(id, OrderStatus.DELETED, DefaultStatus.ACTIVE);
         model.addAttribute("orderDetails", orderDetails);
 
         return "backend/order/detail";
@@ -60,7 +58,7 @@ public class OrderServiceImpl implements OrderService {
         }
         model.addAttribute("order", order);
 
-        var orderDetails = orderDetailRepository.findAllByOrderIdStatusNot(id, OrderStatus.DELETED);
+        var orderDetails = orderDetailRepository.findAllByOrderIdAndStatusNot(id, OrderStatus.DELETED, DefaultStatus.ACTIVE);
         model.addAttribute("orderDetails", orderDetails);
 
         return "frontend/order-success";
